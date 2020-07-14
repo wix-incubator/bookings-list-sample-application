caecd0233387f6a288fa502dd4b642c466b28d0f
Alon Roth <alon@codicate.com>
2020-07-14 11:48:58 +0300

    WIX-4 following the "Guide how to create a wsr app without Yoshi" #5545

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,52 @@
+const path = require('path');
 const { StylableWebpackPlugin } = require('@stylable/webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
     module: {
         rules: [
             {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                    }
+                ]
+            },
+            {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
             },
             {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                loader: 'file-loader',
+                test: /\.scss$/,
+                include: [
+                    path.join(__dirname, 'node_modules/wix-animations'),
+                    path.join(__dirname, 'node_modules/wix-style-react'),
+                    path.join(__dirname, 'node_modules/bootstrap-sass')
+                ],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            localsConvention: 'camelCase',
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]___[hash:base64:5]'
+                            }
+                        }
+                    },
+                    {
+                        loader: 'resolve-url-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                ],
             },
         ],
     },
