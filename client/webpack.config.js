535c5364fd9154e1ca176db2c3462f6bf32c3a86
bureyburey <bar@codicate.com>
2020-07-16 11:07:58 +0300

    sass usage

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                 include: [
                     path.join(__dirname, 'node_modules/wix-animations'),
                     path.join(__dirname, 'node_modules/wix-style-react'),
-                    path.join(__dirname, 'node_modules/bootstrap-sass')
+                    // path.join(__dirname, 'node_modules/bootstrap-sass')
                 ],
                 use: [
                     'style-loader',
