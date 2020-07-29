4893c6a0410de78b4eb484601f640f5cddd3dda5
bureyburey <bar@codicate.com>
2020-07-29 12:59:33 +0300

    removed authorization from network headers

--- client/src/network.js
+++ client/src/network.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: process.env.API_PATH,
     headers: {
-        // 'Authorization': localStorage.getItem('token'),
-        'Authorization': process.env.API_TOKEN,
         'instanceId': sessionStorage.getItem('instanceId')
     }
 });
