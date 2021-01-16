63a75353525b02cf583a1292da0e293ff68eb468
MichalMenachem <michalmanah@gmail.com>
2021-01-17 00:25:06 +0200

    adjust reschedule modal to new design and content

--- client/src/components/RescheduleModal/RescheduleModal.jsx
+++ client/src/components/RescheduleModal/RescheduleModal.jsx
@@ -6,21 +6,24 @@ import {raiseNotification, translate} from '../../utils';
 import RescheduleBox from '../RescheduleBox';
 import RescheduleBoxSkeleton from '../RescheduleBox/RescheduleBoxSkeleton';
 import moment from 'moment-timezone';
+import {range, groupBy} from 'lodash';
 
-const MAX_SLOTS_AMOUNT = 5;
+const START_HOUR_OF_DAY = 5;
+const PARTS_OF_DAY = [{name: 'morning', skeletonBoxes: 3}, {name: 'afternoon', skeletonBoxes: 2}, {name: 'evening', skeletonBoxes: 4}];
 
 @inject('bookingsListStore')
 @observer
 export default class RescheduleModal extends React.PureComponent {
 
     constructor(props) {
-    super(props);
-    this.state = {selectedDate: null};
-  }
+        super(props);
+        this.state = {selectedDate: null};
+    }
 
     _closeModal = () => {
         const {bookingsListStore} = this.props;
         bookingsListStore.setRescheduleModalIsOpen(false);
+        this.setState({selectedDate: null});
     };
 
     _onOk = async () => {
@@ -44,23 +47,49 @@ export default class RescheduleModal extends React.PureComponent {
     _renderSlotsSkeleton = () => {
         return (
             <div className={st(classes.slotsContainer)}>
-                {[...Array(MAX_SLOTS_AMOUNT)].map((_, index) => <RescheduleBoxSkeleton key={index}/>)}
+                {PARTS_OF_DAY.map((dayPart, index) => {
+                    return (
+                        <div key={index} className={st(classes.dayPart)}>
+                            <Text size="medium">{translate(`RescheduleModal.DayParts.${dayPart.name}`)}</Text>
+                            {range(dayPart.skeletonBoxes).map((_, index) => <RescheduleBoxSkeleton key={index}/>)}
+                        </div>
+                    );
+                })}
             </div>
         );
+    };
+
+    _divideSlotsToDayParts = (slots) => {
+        return groupBy(slots, (slot) => {
+            const startHour = moment(slot.start.timestamp).hour();
+            if (startHour >= START_HOUR_OF_DAY && startHour < 12) {
+                return 'morning';
+            } else if (startHour >= 12 && startHour < 17) {
+                return 'afternoon';
+            } else if (startHour >= 17) {
+                return 'evening';
+            }
+        });
     };
 
     _renderSlots = () => {
         const {bookingsListStore} = this.props;
         const {rescheduleModal} = bookingsListStore.store;
         const {slots, selectedSlot} = rescheduleModal;
+        const dividedSlots = this._divideSlotsToDayParts(slots);
         return (
             <div className={st(classes.slotsContainer)}>
-                {
-                    slots.slice(0, MAX_SLOTS_AMOUNT).map((slot, index) => (
-                            <RescheduleBox key={index} onClick={this._setSelectedSlot} isSelected={slot.clientId === (selectedSlot && selectedSlot.clientId)} data={slot}/>
-                        )
-                    )
-                }
+                {PARTS_OF_DAY.map((dayPart, index) => {
+                    return (
+                        <div key={index} className={st(classes.dayPart)}>
+                            <Text size="medium">{translate(`RescheduleModal.DayParts.${dayPart.name}`)}</Text>
+                            {dividedSlots[dayPart.name] ?
+                                dividedSlots[dayPart.name].map((slot, index) =>
+                                    <RescheduleBox key={index} onClick={this._setSelectedSlot} isSelected={slot.clientId === (selectedSlot && selectedSlot.clientId)} data={slot}/>)
+                                : <Text size="medium">No Available Hours</Text>}
+                        </div>
+                    );
+                })}
             </div>
         );
     };
@@ -92,13 +121,23 @@ export default class RescheduleModal extends React.PureComponent {
         const {bookingsListStore} = this.props;
         const {rescheduleModal} = bookingsListStore.store;
         this.setState({selectedDate: value});
-        bookingsListStore.fetchScheduleSlots(rescheduleModal.data.bookedEntity.scheduleId, moment(value).add(5, 'h').toISOString(), moment(value).add(24, 'h').toISOString());
+        bookingsListStore.fetchScheduleSlots(rescheduleModal.data.bookedEntity.scheduleId, moment(value).add(START_HOUR_OF_DAY, 'h').toISOString(), moment(value).add(24, 'h').toISOString());
+    };
+
+    _displayAfterDateSelection = () => {
+        const {bookingsListStore} = this.props;
+        const {rescheduleModal} = bookingsListStore.store;
+        const {loading, slots} = rescheduleModal;
+        const {selectedDate} = this.state;
+        if (selectedDate) {
+            return loading || !slots ? this._renderSlotsSkeleton() : this._renderSlots();
+        }
     };
 
     _renderContent = () => {
         const {bookingsListStore} = this.props;
         const {rescheduleModal} = bookingsListStore.store;
-        const {loading, data, slots, errorMessage} = rescheduleModal;
+        const { data} = rescheduleModal;
         const {selectedDate} = this.state;
         if (!data) {
             return null;
@@ -107,14 +146,14 @@ export default class RescheduleModal extends React.PureComponent {
         const startDate = moment(singleSession ? singleSession.start : setOfSessions.firstSessionStart).format('MMM DD');
         return (
             <Box direction="vertical">
-                <Text size="tiny" style={{padding: '10px 5px'}}>{translate('RescheduleModal.chooseNewSlotLabel', {name: firstName, booking: title, date: startDate})}</Text>
+                <Text size="medium" style={{padding: '10px 5px'}}>{translate('RescheduleModal.chooseNewSlotLabel', {name: firstName, booking: title, date: startDate})}</Text>
                 {this._renderErrorMessage()}
                 <DatePicker
                     value={selectedDate}
                     onChange={this._onDateSelection}
                     excludePastDates
                 />
-                {/* {loading && !slots ? this._renderSlotsSkeleton() : this._renderSlots()} */}
+                {this._displayAfterDateSelection()}
             </Box>
         );
     };
@@ -127,7 +166,7 @@ export default class RescheduleModal extends React.PureComponent {
             <Box>
                 <Modal isOpen={isOpen} onRequestClose={this._closeModal} shouldCloseOnOverlayClick={true}>
                     <MessageBoxFunctionalLayout
-                        width={'500px'}
+                        width={'40vw'}
                         title={translate('RescheduleModal.title')}
                         confirmText={translate('RescheduleModal.confirmButtonText')}
                         cancelText={translate('RescheduleModal.cancelButtonText')}
