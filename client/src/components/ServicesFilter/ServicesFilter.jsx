d9da5edf659edf49534776041e7e7991374270e6
MichalMenachem <michalmanah@gmail.com>
2021-01-21 16:10:32 +0200

    add comment explaining toJS

--- client/src/components/ServicesFilter/ServicesFilter.jsx
+++ client/src/components/ServicesFilter/ServicesFilter.jsx
@@ -33,6 +33,7 @@ export default class ServicesFilter extends React.Component {
 
     _onServiceFilterClose = () => {
         const {onFilterChanged, filters} = this.props;
+        // toJS recursively converts an observable object to a javascript structure
         if (!isEqual(filters.services.toJS().sort(), this.state.selectedServices.sort())) {
             onFilterChanged('services', this.state.selectedServices);
         }
